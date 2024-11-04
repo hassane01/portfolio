d6d57c9ff118c568a985b0799d1a3311b0df3bb4
hassane01 <oubakassehassane7@gmail.com>
2024-11-04 09:35:32 +0100

    add a new project to the works section

--- src/assets/Data/Workdata.js
+++ src/assets/Data/Workdata.js
@@ -2,6 +2,8 @@ import cryptoimg from '../../assets/Cryptoprojct.jpeg';
 import weatherimg from '../../assets/watercolor.jpeg';
 import weathergif from '../../assets/videos/weathervid.gif';
 import cryptogif from '../../assets/videos/cryptovid.gif';
+import robotimg from '../../assets/robot.png'
+import medernegif from '../../assets/videos/modernevid.gif'
 
 export const data = [
   {
@@ -25,5 +27,16 @@ export const data = [
     title: 'weatherAppTitle', // translation key
     description: 'weatherAppDescription', // translation key
     skills: 'weatherAppSkills', // translation key
+  },
+  {
+    id: 3,
+    name: 'Modernewebsite', // translation key
+    demolink: 'https://baank-figma.netlify.app/',
+    repolink: 'https://github.com/hassane01/Bank-figma-design',
+    image: robotimg,
+    gif: medernegif,
+    title: 'moderneAppTitle', // translation key
+    description: 'moderneAppDescription', // translation key
+    skills: 'moderneAppSkills', // translation key
   },
 ];
